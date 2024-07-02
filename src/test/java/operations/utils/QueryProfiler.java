8fc5cc53fa89635653213e95883709126e03ccfe
mackchay <egoregor2068@gmail.com>
2024-07-02 17:18:26 +0700

    Profiling prototype is done!

--- src/test/java/operations/utils/QueryProfiler.java
+++ src/test/java/operations/utils/QueryProfiler.java
@@ -2,7 +2,6 @@ package operations.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.HashMap;
